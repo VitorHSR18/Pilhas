ca1c68041df0c0038549933900bae962d1f58e82
VitorHSR18 <136871451+VitorHSR18@users.noreply.github.com>
2024-03-19 11:08:23 -0300

    Update TrabalhoPaciencia.cpp

--- TrabalhoPaciencia.cpp
+++ TrabalhoPaciencia.cpp
@@ -48,30 +48,30 @@ void Moldura (int ci, int li, int cf, int lf, int cort) //moldura
 void formulario(void) // formulario
 {
 	system("cls");
-	Moldura(1,2,100,27,7); // BORDA
-	Moldura(1,27,100,27,0);
-		gotoxy(37,4);
+	//Moldura(1,2,100,27,9); // BORDA
+	//Moldura(1,27,100,27,0); // tirar o final da borda
+		gotoxy(25,4);
 		textcolor(7);
 		printf("# # # Paciencia # # #");
-	Moldura(2,3,99,5,7);
-	Moldura(2,6,99,14,7); // Titulo
+	Moldura(2,3,70,5,9); // Titulo
+	Moldura(2,6,70,12,9); 
 	gotoxy(9,13);
 	textcolor(7);
-	printf("Tecle Enter");
-	Moldura(3,7,16,11,12); 
-	Moldura(17,7,30,11,7);
-	Moldura(31,6,31,12,7);
-	Moldura(33,7,46,11,12);
-	Moldura(47,7,60,11,7);
-	Moldura(61,7,74,11,12);
-	Moldura(75,7,88,11,7);
+	Moldura(3,7,9,11,12); 
+	Moldura(11,7,17,11,7);
+	Moldura(33,7,39,11,12);
+	Moldura(41,7,47,11,7);
+	Moldura(49,7,55,11,12);
+	Moldura(57,7,63,11,7);
 		gotoxy(30,25);
 		textcolor(6);	
 }
 
-void main(void)
+int main(void)
 {
 	//TpPilha1 P1[14];
 	
 	formulario();
+	
+	
 }
